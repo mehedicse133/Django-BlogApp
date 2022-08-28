e0f5a8e5dde329fb695e99c19ff0aafeaf874c55
mehedicse133 <mehedicse047@gmail.com>
2022-08-28 12:54:00 +0600

    add post comment features

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from .models import Post
+from django.shortcuts import render,redirect
+from .models import Post, Comment
+from .forms import ContactForm
+
 
 def home(request):
     posts = Post.objects.all
@@ -11,7 +13,18 @@ def home(request):
 
 def detail(request,slug):
     post = Post.objects.get(slug=slug)
+
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('detail', slug = post.slug)
+    else: 
+        form = ContactForm()     
     context = {
-        'post': post
+        'post': post,
+        'form': form
     }
     return render(request,'blog_details.html',context)
